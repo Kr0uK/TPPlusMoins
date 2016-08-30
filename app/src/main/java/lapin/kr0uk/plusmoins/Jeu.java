fee4888145a680e4cc6db8053c60424292c4f00f
Kr0uK <boris.motz@gmail.com>
2016-08-30 15:31:56 +0200

    merguez

--- app/src/main/java/lapin/kr0uk/plusmoins/Jeu.java
+++ app/src/main/java/lapin/kr0uk/plusmoins/Jeu.java
@@ -2,12 +2,39 @@ package lapin.kr0uk.plusmoins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Jeu extends AppCompatActivity {
+    Random rnd = new Random ();
+    int nbGenere = rnd.nextInt(100)+1;
+    int reponse;
+    int nbErreurs = 0;
+
+    TextView txtStatut ;
+    TextView txtResultat;
+    EditText txtInputReponse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jeu);
+
+        Button btnValider = (Button) findViewById(R.id.btnValider);
+        txtStatut  = (TextView) findViewById(R.id.txtStatut);
+        txtResultat = (TextView) findViewById(R.id.txtResultat);
+        txtInputReponse = (EditText) findViewById(R.id.txtInputReponse);
+
+
+    }
+
+
+    public void btn_click(View view) {
+
     }
 }
