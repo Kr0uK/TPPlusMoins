629484617b5cf67b04a9ae353cfa05100f1b9d3f
Kr0uK <boris.motz@gmail.com>
2016-08-30 16:08:09 +0200

    merguez

--- app/src/main/java/lapin/kr0uk/plusmoins/Jeu.java
+++ app/src/main/java/lapin/kr0uk/plusmoins/Jeu.java
@@ -2,22 +2,27 @@ package lapin.kr0uk.plusmoins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class Jeu extends AppCompatActivity {
+
+    // On déclare les variables
     Random rnd = new Random ();
     int nbGenere = rnd.nextInt(100)+1;
     int reponse;
     int nbErreurs = 0;
+    String debug = Integer.toString(nbGenere);
 
-    TextView txtStatut ;
-    TextView txtResultat;
-    EditText txtInputReponse;
+    TextView txtStatut ; // texte pour vérifier la victoire/défaite
+    TextView txtResultat; // texte qui indique plus/moins
+    EditText txtInputReponse; // réponse de l'user
 
 
     @Override
@@ -25,16 +30,69 @@ public class Jeu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jeu);
 
+        // on lie le XML à Java
         Button btnValider = (Button) findViewById(R.id.btnValider);
-        txtStatut  = (TextView) findViewById(R.id.txtStatut);
+        txtStatut = (TextView) findViewById(R.id.txtStatut);
         txtResultat = (TextView) findViewById(R.id.txtResultat);
         txtInputReponse = (EditText) findViewById(R.id.txtInputReponse);
 
 
+        // Listener sur le bouton
+        btnValider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+
+                /*
+                    on met la bonne valeur en debug
+                    Log.w("TAG",debug);
+                    Log.d("TAG",debug);
+                    Log.e("TAG",debug);
+                    */
+
+                    reponse = Integer.parseInt(txtInputReponse.getText().toString());
+                    if (reponse < 1 || reponse > 100) { // on vérifie que le résulat soit bien dans les bonnes valeurs
+
+                        Toast.makeText(Jeu.this, "Resaisissez un chiffre entre 1 et 100", Toast.LENGTH_LONG).show();
+                    } else {
+
+                        verif(reponse);
+                    }
+
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Saisissez un nombre entre 1 et 100.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
+    private void  verif(int reponse) { // On vérifie que ce soit juste ou non
+        String strReponse = "";
 
-    public void btn_click(View view) {
+
+        if (nbErreurs<5) {
+
+            nbErreurs++;
+
+            if (reponse == nbGenere ) { // on vérifie si la réponse est juste
+                strReponse = "Félicitations !";
+            } else if (reponse < nbGenere) { //
+                strReponse = "Le bon numéro est PLUS GRAND que celui que vous avez tapé.";
+            }
+            else {
+                strReponse = "Le bon numéro est PLUS PETIT que celui que vous avez tapé.";
+            }
+
+        }
+        else {
+            strReponse = "Vous avez perdu, le bon numéro était "+ nbGenere;
+        }
+        String sEssais = "Vous êtes à " +  nbErreurs + " Essai(s)";
+        txtResultat.setText(strReponse);
+        txtStatut.setText(sEssais);
 
     }
 }
+
+
+
